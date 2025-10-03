a538ac3ab8d7005eb577613572b11fa6b431b9bb
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:46:59 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HIM';
+export let nameSongs = [
+	"JOIN ME IN DEATH", /*1*/
+	"IN JOY AND SORROW", /*2*/
+	"WINGS OF A BUTTERFLY", /*3*/
+	"KILLING LONELINESS", /*4*/
+	"HEARTKILLER", /*5*/
+	"SCARED TO DEATH", /*6*/
+	"FOR YOU", /*7*/
+	"DON'T FEAR THE REAPER", /*8*/
+	"THE KISS OF DAWN", /*9*/
+	"BLEED WELL", /*10*/
+	"GONE WITH THE SIN", /*11*/
+	"RIGHT HERE IN MY ARMS", /*12*/
+	"JOIN ME", /*13*/
+	"BURIED ALIVE BY LOVE", /*14*/
+	"ALL LIPS GO BLUE", /*15*/
+	"INTO THE NIGHT", /*16*/
+	"SOLITARY MAN", /*17*/
+	" POISON GIRL", /*18*/
+	"BURN", /*19*/
+	"THE FUNERAL OF HEARTS", /*20*/
+]
